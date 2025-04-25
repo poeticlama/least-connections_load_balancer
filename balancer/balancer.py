12c01b32ea834411b741c38043de4454b2c6b969
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-04-26 02:22:23 +0300

    Added multithreading and comments

--- balancer/balancer.py
+++ balancer/balancer.py
@@ -1,6 +1,7 @@
 import argparse
 import socket
 import os
+import threading
 
 IP = '127.0.0.1'
 port = 8000
@@ -10,11 +11,19 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(current_dir, 'received_image.jpg')
 
 
+def least_connection():
+    # Here is the place for main algorithm
+    pass
+
+
 def on_accept():
+    # Here is what we should do when we got a new response from a client.
+    # Somewhere here we should call our algorithm
     pass
 
 
 def on_recv():
+    # Some logic for sending a picture to the client
     pass
 
 
@@ -53,33 +62,55 @@ def create_sockets():
     return sockets
 
 
+def handle_request(conn, address):
+    data = conn.recv(1024).decode()
+    number = int(data)
+
+
 def main():
     backend_sockets = create_sockets()
 
-    # Creating balancer socket
+    # Connect to frontend app
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        num = 2
-        try:
-            # Connecting to backend app
-            sock.connect((IP, port))
-            request = \
-                f'GET /images/{num} HTTP/1.1\r\n'\
-                f'Host: {IP}:{port}\r\n'\
-                'Connection: close\r\n'\
-                '\r\n'
-            sock.sendall(request.encode('utf-8'))
-            response = b''
-            while True:
-                chunk = sock.recv(1024)
-                if not chunk:
-                    break
-                response += chunk
-            header_end = response.find(b"\r\n\r\n")
-            headers = response[:header_end].decode()
-            image_data = response[header_end+4:]
+        # Some configuration for socket
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((IP, port))
+        sock.listen()
 
-            with open(path, "wb") as f:
-                f.write(image_data)
+        try:
+            while True:
+                # Accepting a new connection from client
+                conn, addr = sock.accept()
+
+                # Implementing multithreading for different responses
+                thread = threading.Thread(
+                    target=on_accept,
+                    args=(conn, addr)
+                )
+                thread.start()
+
+                # # Some code to be placed in on_accept() function
+                # # Num is just an index of picture that we got from client
+                # num = 2
+                # request = \
+                #     f'GET /images/{num} HTTP/1.1\r\n'\
+                #     f'Host: {IP}:{port}\r\n'\
+                #     'Connection: close\r\n'\
+                #     '\r\n'
+                # sock.sendall(request.encode('utf-8'))
+                # response = b''
+                # while True:
+                #     chunk = sock.recv(1024)
+                #     if not chunk:
+                #         break
+                #     response += chunk
+                # header_end = response.find(b"\r\n\r\n")
+                # headers = response[:header_end].decode()
+                # image_data = response[header_end+4:]
+                #
+                # with open(path, "wb") as f:
+                #     f.write(image_data)
+
         except:
             print("Error")
         finally:
