df1e535157335c610a3bdb1bb9eecde98ea802a9
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-04-26 16:19:51 +0300

    Added comments

--- frontend-app/main.js
+++ frontend-app/main.js
@@ -1,9 +1,15 @@
 const url = "127.0.0.1:8000"
 
 document.getElementById("main-button").addEventListener("click", async () => {
+    // Actions after clicking a button
+
+    // Randomly choosing a number of picture
     const image_num = Math.ceil(Math.random() * 4)
+
+    // Changing text in image space
     document.querySelector('.picture').children[0].innerHTML = 'Waiting for server response...'
 
+    // Sending a request to balancer
     const response = await fetch(new Request(url, {
         method: "GET",
         headers: {
